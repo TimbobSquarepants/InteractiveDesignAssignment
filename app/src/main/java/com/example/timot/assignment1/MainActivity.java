2cb01819a7a574ac645ed8b038f3ef5d9bd4b5e9
TimboAuS <TimothyDavidFinn1@gmail.com>
2018-05-08 16:54:03 +0930

    Testing Android Github Support

--- app/src/main/java/com/example/timot/assignment1/MainActivity.java
+++ app/src/main/java/com/example/timot/assignment1/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String test = "Hello World";
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
