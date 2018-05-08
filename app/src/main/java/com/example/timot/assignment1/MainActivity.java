24efe4fe4404921035cdb043e6ae7b11dddae2f2
TimboAuS <TimothyDavidFinn1@gmail.com>
2018-05-08 16:18:16 +0930

    Added base functionality to project.

---
+++ app/src/main/java/com/example/timot/assignment1/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.timot.assignment1;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Help not implemented in this current wireframe", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //setting up tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_bar);
+        ViewPager pager = (ViewPager) findViewById(R.id.viewpager);
+
+        //still the tabs
+        TabPageAdapter tabPageAdapter = new TabPageAdapter(getSupportFragmentManager());
+        pager.setAdapter(tabPageAdapter);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.create_security_report) {
+            //  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new CreateReport()).commit();
+            Toast.makeText(this, "Feature Not Implemented Yet s", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.view_security_report) {
+            // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ViewReports()).commit();
+            Toast.makeText(this, "Feature Not Implemented Yet s", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.find_security_kiosk) {
+            // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FindKiosk()).commit();
+            Toast.makeText(this, "Feature Not Implemented Yet s", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.help) {
+            Toast.makeText(this, "Feature Not Implemented Yet s", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.sign_out) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Sign Out")
+                    .setMessage("Are you sure you want to sign out?").setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+            }).setNegativeButton("NO", null).show();
+
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
---
+++ app/src/main/java/com/example/timot/assignment1/TabPageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.timot.assignment1;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPageAdapter extends FragmentStatePagerAdapter {
+
+    String[] tabArray = new String[]{"Create Security Report", "View Security Reports", "Find Security Kiosk"};
+    Integer tabNumber = 3;
+
+    public TabPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabArray[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+
+            case 0:
+                CreateReport create = new CreateReport();
+                return create;
+
+            case 1:
+                ViewReports view = new ViewReports();
+                return view;
+
+            case 2:
+                FindKiosk find = new FindKiosk();
+                return find;
+
+
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabNumber;
+    }
+}
